e0e98820d685b958957311902f34a39eead28cf6
Andrey Kartashov <gorilych@gmail.com>
2013-07-31 11:34:22 +0700

    Moved su.py out of ssh.py to remote-scripts directory

---
+++ checks/remote-scripts/bin32/su.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import pexpect
+import sys
+import os
+import threading
+
+sutype      = sys.argv[1]
+password    = sys.argv[2]
+stderr_fifo = sys.argv[3]
+stdout_fifo = sys.argv[4]
+command     = sys.argv[5]
+
+def read_from_to(fifo_name, fout):
+  fifo = os.fdopen(os.open(fifo_name, os.O_RDONLY), 'r')
+  while True:
+    line = fifo.readline()
+    if not line: break
+    fout.write(line)
+  fifo.close()
+
+if command == 'shell':
+  if password == '' and sutype == 'sudo':
+    sucmd = 'sudo'
+    suargs = ['su', '-']
+  if not password == '':
+    if sutype == 'su'  :
+      sucmd = 'su'
+    if sutype == 'su'  :
+      sucmd = 'su'
+      suargs = ['-']
+    elif sutype == 'sudo':
+      sucmd = 'sudo'
+      suargs = ['-p', 'password: ', '--', 'su', '-']
+  try:
+    pexpect.WINHEIGHT = int(stdout_fifo)
+    pexpect.WINWIDTH  = int(stderr_fifo)
+  except:
+    pass
+  child = pexpect.spawn(sucmd, suargs)
+  if not password == '':
+    child.expect_exact('assword: ')
+    child.sendline(password)
+  child.interact()
+else:
+  if password == '' and sutype == 'sudo':
+    sucmd = 'sudo'
+    suargs = ['su']
+  if not password == '':
+    if sutype == 'sudo':
+      sucmd = 'sudo'
+      suargs = ['-p', 'password: ', 'su']
+    if sutype == 'su':
+      sucmd = 'su'
+      suargs = []
+  suargs += ['-', '-c', '{ %s; } 1>%s 2>%s' % (command, stdout_fifo, stderr_fifo)]
+
+  stdout_th = threading.Thread(name='stdout', target=read_from_to, args=(stdout_fifo, sys.stdout))
+  stderr_th = threading.Thread(name='stderr', target=read_from_to, args=(stderr_fifo, sys.stderr))
+
+  stdout_th.start()
+  stderr_th.start()
+
+  # cleanup cached credentials
+  if sutype == 'sudo':
+    child = pexpect.spawn('sudo -k')
+    child.close()
+  child = pexpect.spawn(sucmd, suargs)
+  if not password == '':
+    child.expect('assword: ')
+    child.sendline(password)
+  child.expect_exact(pexpect.EOF)
+  child.close()
+  exitcode = child.exitstatus
+
+  stdout_th.join()
+  stderr_th.join()
+
+  sys.exit(child.exitstatus)
+
--- hwswa2/ssh.py
+++ hwswa2/ssh.py
@@ -8,6 +8,7 @@ import signal
 import paramiko
 import hwswa2.interactive as interactive
 import hwswa2.aux as aux
+from hwswa2.globals import config
 from hwswa2.log import debug
 
 ssh_timeout = 30
@@ -205,99 +206,16 @@ def check_reboot(server, timeout=300):
 
 def prepare_su(server):
   """Copies su.py to remote server and returns path to containing directory"""
-  su_py = """#!/usr/bin/env python
-import pexpect
-import sys
-import os
-import threading
-
-sutype      = sys.argv[1]
-password    = sys.argv[2]
-stderr_fifo = sys.argv[3]
-stdout_fifo = sys.argv[4]
-command     = sys.argv[5]
-
-def read_from_to(fifo_name, fout):
-  fifo = os.fdopen(os.open(fifo_name, os.O_RDONLY), 'r')
-  while True:
-    line = fifo.readline()
-    if not line: break
-    fout.write(line)
-  fifo.close()
-
-if command == 'shell':
-  if password == '' and sutype == 'sudo':
-    sucmd = 'sudo'
-    suargs = ['su', '-']
-  if not password == '':
-    if sutype == 'su'  : 
-      sucmd = 'su'
-      suargs = ['-']
-    elif sutype == 'sudo':
-      sucmd = 'sudo'
-      suargs = ['-p', 'password: ', '--', 'su', '-']
-  try:
-    pexpect.WINHEIGHT = int(stdout_fifo)
-    pexpect.WINWIDTH  = int(stderr_fifo)
-  except:
-    pass
-  child = pexpect.spawn(sucmd, suargs)
-  if not password == '':
-    child.expect_exact('assword: ')
-    child.sendline(password)
-  child.interact()
-else:
-  if password == '' and sutype == 'sudo':
-    sucmd = 'sudo'
-    suargs = ['su']
-  if not password == '':
-    if sutype == 'sudo':
-      sucmd = 'sudo'
-      suargs = ['-p', 'password: ', 'su']
-    if sutype == 'su':
-      sucmd = 'su'
-      suargs = []
-  suargs += ['-', '-c', '{ %s; } 1>%s 2>%s' % (command, stdout_fifo, stderr_fifo)]
-
-  stdout_th = threading.Thread(name='stdout', target=read_from_to, args=(stdout_fifo, sys.stdout))
-  stderr_th = threading.Thread(name='stderr', target=read_from_to, args=(stderr_fifo, sys.stderr))
-
-  stdout_th.start()
-  stderr_th.start()
-
-  # cleanup cached credentials
-  if sutype == 'sudo':
-    child = pexpect.spawn('sudo -k')
-    child.close()
-  child = pexpect.spawn(sucmd, suargs)
-  if not password == '':
-    child.expect('assword: ')
-    child.sendline(password)
-  child.expect_exact(pexpect.EOF)
-  child.close()
-  exitcode = child.exitstatus
-
-  stdout_th.join()
-  stderr_th.join()
-
-  sys.exit(child.exitstatus)
-"""
+  su_py      = os.path.join(config['rscriptdir'], 'bin32', 'su.py')
+  pexpect_py = os.path.join(config['rscriptdir'], 'bin32', 'pexpect.py')
   # create directory
   supath = mktemp(server, template='su.XXXX', path='/tmp')
-  # copy pexpect.py
-  import pexpect
-  pexpect_file = pexpect.__file__
-  if pexpect_file.endswith('.pyc'):
-    pexpect_file = pexpect_file[:-1]
-  put(server, pexpect_file, supath)
-  # copy su.py
-  su_py_path = os.path.join(supath, 'su.py')
-  write(server, su_py_path, su_py)
+  put(server, pexpect_py, supath)
+  put(server, su_py, supath)
   # prepare stdout and stderr fifos:
   exec_cmd(server, "mkfifo %s" % os.path.join(supath, 'stdout'), privileged=False)
   exec_cmd(server, "mkfifo %s" % os.path.join(supath, 'stderr'), privileged=False)
   server['supath'] = supath
-  return supath
 
 def prepare_su_cmd(server, cmd):
   if not ('su' in server['account'] or 'sudo' in server['account']):
