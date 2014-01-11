6eac9eb3699b7271af3fd5b7f3368a1fc8d4af26
Andrey Kartashov <gorilych@gmail.com>
2014-01-11 15:32:38 +0700

    hwswa2.aux: added decorator @threaded

--- hwswa2/aux.py
+++ hwswa2/aux.py
@@ -39,6 +39,33 @@ def passbyval(func):
     return func(*cargs, **ckargs)
   return _new_function_with_args_passed_by_value
 
+
+def threaded(f, daemon=False):
+  """Decorator for threaded functions"""
+  import Queue, threading
+
+  def wrapped_f(q, *args, **kwargs):
+    '''this function calls the decorated function and puts the 
+       result in a queue'''
+    ret = f(*args, **kwargs)
+    q.put(ret)
+
+  def wrap(*args, **kwargs):
+    '''this is the function returned from the decorator. It fires off
+       wrapped_f in a new thread and returns the thread object with
+       the result queue attached'''
+
+    q = Queue.Queue()
+
+    t = threading.Thread(target=wrapped_f, args=(q,)+args, kwargs=kwargs)
+    t.daemon = daemon
+    t.start()
+    t.result_queue = q        
+    return t
+
+  return wrap
+
+
 def shell_escape(string):
     """
     Escape double quotes, backticks and dollar signs in given ``string``.
