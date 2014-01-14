88a8a12b0edde77006d7abbdafc03117eecc2ccb
Andrey Kartashov <gorilych@gmail.com>
2014-01-14 09:40:02 +0700

    Reworked su.py to handle window resize

--- checks/remote-scripts/bin32/su.py
+++ checks/remote-scripts/bin32/su.py
@@ -3,6 +3,10 @@ import pexpect
 import sys
 import os
 import threading
+import struct
+import signal
+import fcntl
+import termios
 
 sutype      = sys.argv[1]
 password    = sys.argv[2]
@@ -19,6 +23,16 @@ def read_from_to(fifo_name, fout):
     fout.write(line)
   fifo.close()
 
+def term_winsz():
+  '''Return terminal window size (height, width)'''
+  winsz_fmt = "HHHH"
+  winsz_arg = " "*struct.calcsize(winsz_fmt)
+  if not sys.stdin.isatty():
+    #raise type("NotConnectToTTYDevice", (Exception,), {})()
+    return (25,80)
+  return struct.unpack(winsz_fmt, fcntl.ioctl(sys.stdin, termios.TIOCGWINSZ, winsz_arg))[:2]
+
+
 if command == 'shell':
   if password == '' and sutype == 'sudo':
     sucmd = 'sudo'
@@ -32,16 +46,19 @@ if command == 'shell':
     elif sutype == 'sudo':
       sucmd = 'sudo'
       suargs = ['-p', 'password: ', '--', 'su', '-']
+  old_handler = signal.getsignal(signal.SIGWINCH)
   try:
-    pexpect.WINHEIGHT = int(stdout_fifo)
-    pexpect.WINWIDTH  = int(stderr_fifo)
-  except:
-    pass
-  child = pexpect.spawn(sucmd, suargs, timeout=timeout)
-  if not password == '':
-    child.expect_exact('assword: ')
-    child.sendline(password)
-  child.interact()
+    def on_win_resize(signum, frame):
+      child.setwinsize(*term_winsz())
+    signal.signal(signal.SIGWINCH, on_win_resize)
+    child = pexpect.spawn(sucmd, suargs, timeout=timeout)
+    child.setwinsize(*term_winsz())
+    if not password == '':
+      child.expect_exact('assword: ')
+      child.sendline(password)
+    child.interact()
+  finally:
+    signal.signal(signal.SIGWINCH, old_handler)
 else:
   if password == '' and sutype == 'sudo':
     sucmd = 'sudo'
@@ -53,28 +70,41 @@ else:
     if sutype == 'su':
       sucmd = 'su'
       suargs = []
-  suargs += ['-', '-c', '{ %s; } 1>%s 2>%s' % (command, stdout_fifo, stderr_fifo)]
-
-  stdout_th = threading.Thread(name='stdout', target=read_from_to, args=(stdout_fifo, sys.stdout))
-  stderr_th = threading.Thread(name='stderr', target=read_from_to, args=(stderr_fifo, sys.stderr))
-
-  stdout_th.start()
-  stderr_th.start()
-
   # cleanup cached credentials
   if sutype == 'sudo':
     child = pexpect.spawn('sudo -k')
     child.close()
+
+  if sys.stdin.isatty():
+    suargs += ['-', '-c', '%s' % command]
+  else:
+    suargs += ['-', '-c', '{ %s; } 1>%s 2>%s' % (command, stdout_fifo, stderr_fifo)]
+    stdout_th = threading.Thread(name='stdout', target=read_from_to, args=(stdout_fifo, sys.stdout))
+    stderr_th = threading.Thread(name='stderr', target=read_from_to, args=(stderr_fifo, sys.stderr))
+    stdout_th.start()
+    stderr_th.start()
+
   child = pexpect.spawn(sucmd, suargs)
   if not password == '':
     child.expect('assword: ')
     child.sendline(password)
-  child.expect_exact(pexpect.EOF)
-  child.close()
+
+  if sys.stdin.isatty():
+    old_handler = signal.getsignal(signal.SIGWINCH)
+    try:
+      def on_win_resize(signum, frame):
+        child.setwinsize(*term_winsz())
+      signal.signal(signal.SIGWINCH, on_win_resize)
+      child.setwinsize(*term_winsz())
+      child.interact()
+    finally:
+      signal.signal(signal.SIGWINCH, old_handler)
+  else:
+    child.expect_exact(pexpect.EOF)
+    child.close()
+    stdout_th.join()
+    stderr_th.join()
+
   exitcode = child.exitstatus
-
-  stdout_th.join()
-  stderr_th.join()
-
   sys.exit(child.exitstatus)
 
