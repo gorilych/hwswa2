254b1504a1835e5b5acd01ecd519a341bd74a911
Andrey Kartashov <gorilych@gmail.com>
2014-06-20 10:19:50 +0700

    Bugfix: rewrote configspec to work with python 2.6

--- hwswa2/globals.py
+++ hwswa2/globals.py
@@ -7,14 +7,14 @@ if getattr(sys, 'frozen', False):
 elif __file__:
     apppath = os.path.abspath(os.path.dirname(os.path.realpath(sys.argv[0])))
 
-configspec = ConfigObj(["debug = boolean()",
-                        "check_reboot = boolean()",
-                        "ssh_timeout = integer(0,7200,default=20)"
-                        "[firewall]",
-                        "send_timeout = float(0, 99, default=1)",
-                        "max_open_sockets = integer(1,10000,default=100)",
-                        ],
-                       _inspec=True, list_values=False)
+configspec = ConfigObj(_inspec=True, list_values=False)
+configspec['debug'] = 'boolean()'
+configspec['check_reboot'] = 'boolean()'
+configspec['ssh_timeout'] = 'integer(0,7200,default=30)'
+configspec['firewall'] = {
+    'send_timeout': 'float(0, 99, default=1)',
+    'max_open_sockets': 'integer(1,10000,default=100)',
+    }
 
 # default parameters
 config = {'configdir': apppath + '/' + 'config',
@@ -22,7 +22,7 @@ config = {'configdir': apppath + '/' + 'config',
           'checksdir': apppath + '/' + 'checks',
           'debug': False,
           'check_reboot': False,
-          'ssh_timeout': 20}
+          'ssh_timeout': 30}
 config['configfile'] = config['configdir'] + '/' + 'main.cfg'
 config['serversfile'] = config['configdir'] + '/' + 'servers.yaml'
 config['networksfile'] = config['configdir'] + '/' + 'networks.yaml'
