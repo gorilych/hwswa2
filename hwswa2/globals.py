00b92e95c925b1154e3892e417eade644da47a0a
Andrey Kartashov <gorilych@gmail.com>
2014-06-20 09:39:41 +0700

    Added validation for ssh_timeout, send_timeout, max_open_sockets to convert it to float/integer

--- hwswa2/globals.py
+++ hwswa2/globals.py
@@ -7,8 +7,13 @@ if getattr(sys, 'frozen', False):
 elif __file__:
     apppath = os.path.abspath(os.path.dirname(os.path.realpath(sys.argv[0])))
 
-configspec = ConfigObj(["debug=boolean()",
-                        "check_reboot=boolean()"],
+configspec = ConfigObj(["debug = boolean()",
+                        "check_reboot = boolean()",
+                        "ssh_timeout = integer(0,7200,default=20)"
+                        "[firewall]",
+                        "send_timeout = float(0, 99, default=1)",
+                        "max_open_sockets = integer(1,10000,default=100)",
+                        ],
                        _inspec=True, list_values=False)
 
 # default parameters
